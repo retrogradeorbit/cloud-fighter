3ab234f33ab907e04c46aecf85167a5d5a99c052
Crispin Wellington <retrogradeorbit@gmail.com>
2016-08-01 15:25:48 +0800

    dependencies

--- project.clj
+++ project.clj
@@ -9,7 +9,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.89"]
                  [org.clojure/core.async "0.2.385"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [infinitelives/infinitelives.pixi "0.1.1-SNAPSHOT"]
+                 [binaryage/devtools "0.7.2"]
+                 ]
 
   :plugins [[lein-figwheel "0.5.4-7"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
