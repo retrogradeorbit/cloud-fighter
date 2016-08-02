75c941e840dd5051604da023e09ad6e910d5f682
Crispin Wellington <retrogradeorbit@gmail.com>
2016-08-02 21:56:54 +0800

    implaement basic bullet

--- src/cloud_fighter/game.cljs
+++ src/cloud_fighter/game.cljs
@@ -31,6 +31,15 @@
              (events/is-pressed? :down) 1
              :default 0)))))
 
+(defn fire? []
+  (or
+   (events/is-pressed? :z)
+   (events/is-pressed? :space)
+   (gp/button-pressed? 0 :a)
+   (gp/button-pressed? 0 :b)
+   (gp/button-pressed? 0 :x)
+   (gp/button-pressed? 0 :y)))
+
 (defn turn-dir [heading]
   (let [dir (direction)
         dot (vec2/dot (vec2/rotate-90 heading) dir)]
@@ -49,16 +58,34 @@
       :default
       :none)))
 
+(defn spawn-bullet! [canvas heading speed lifetime]
+  (let [update (-> heading vec2/unit
+                   (vec2/scale speed))]
+    (go
+      (m/with-sprite canvas :bullets
+        [bullet (s/make-sprite :bullet :scale 4 :rotation (+ heading Math/PI) :x 0 :y 0)]
+        (loop [n lifetime
+               pos (vec2/vec2 0 0)]
+          (s/set-pos! bullet pos)
+          (<! (e/next-frame))
+
+          (when (pos? n)
+            (recur (dec n) (vec2/add pos update)))))))
+)
+
 (defn run [canvas player]
   (go
     ;; loop forever
     (loop [heading (vec2/vec2 0 -1)]
       (parallax/update! (vec2/scale heading 0.5))
       (s/set-rotation! player (vec2/heading (vec2/rotate-90 heading)))
+
+      (when (fire?)
+        (spawn-bullet! canvas heading 10 60))
+
       (<! (e/next-frame))
-      (let [turn (turn-dir heading)]
-        (recur (vec2/rotate heading
-                            (case turn
-                              :right rotate-speed
-                              :left (- rotate-speed)
-                              :none 0)))))))
+      (recur (vec2/rotate heading
+                          (case (turn-dir heading)
+                            :right rotate-speed
+                            :left (- rotate-speed)
+                            :none 0))))))
