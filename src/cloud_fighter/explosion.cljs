d4943b5101d1602a9c676e8c22e92c0d7e5cceb9
Crispin Wellington <retrogradeorbit@gmail.com>
2016-08-07 19:37:02 +0800

    explosion han be large or small

--- src/cloud_fighter/explosion.cljs
+++ src/cloud_fighter/explosion.cljs
@@ -13,36 +13,44 @@
                        :explode-5 :explode-6
                        :explode-7])
 
+(def small-explosion-frames [:small-explosion-1
+                             :small-explosion-2
+                             :small-explosion-3
+                             :small-explosion-4
+                             :small-explosion-5
+                             :small-explosion-6])
+
 (def explosion-speed 6)
 
-(defn explosion [canvas entity move?]
+(defn explosion [canvas entity move? small?]
   ;(sound/play-sound (keyword (str "explode-" (rand-int 10))) 0.5 false)
-  (go
-    (let [initial-pos (s/get-pos entity)
-          x (vec2/get-x initial-pos)
-          y (vec2/get-y initial-pos)
-          frames (count explosion-frames)
-          total-frames (* frames explosion-speed)
-          ]
-      (m/with-sprite canvas :player
-        [explosion (s/make-sprite (first explosion-frames)
-                                  :scale 4 :x x :y y
-                                  ;:rotation (* (rand) Math/PI 2)
-                                  )]
-        (loop [n 0
-               pos initial-pos]
-          (s/set-texture! explosion (get explosion-frames (int (/ n explosion-speed)) :explode-7))
-          (s/set-pos! explosion pos)
+  (let [frameset (if small? small-explosion-frames explosion-frames)]
+    (go
+      (let [initial-pos (s/get-pos entity)
+            x (vec2/get-x initial-pos)
+            y (vec2/get-y initial-pos)
+            frames (count frameset)
+            total-frames (* frames explosion-speed)
+            ]
+        (m/with-sprite canvas :player
+          [explosion (s/make-sprite (first frameset)
+                                    :scale 4 :x x :y y
+                                        ;:rotation (* (rand) Math/PI 2)
+                                    )]
+          (loop [n 0
+                 pos initial-pos]
+            (s/set-texture! explosion (get frameset (int (/ n explosion-speed)) (last frameset)))
+            (s/set-pos! explosion pos)
 
-          ;; when explosion is maximum size, we can disappear the
-          ;; underlying entity
-          (when (= 3 n)
-            (s/set-visible! entity false))
+            ;; when explosion is maximum size, we can disappear the
+            ;; underlying entity
+            (when (= 3 n)
+              (s/set-visible! entity false))
 
-          (<! (e/next-frame))
+            (<! (e/next-frame))
 
-          (when (< n total-frames)
-            (recur (inc n)
-                   (if move?
-                     (vec2/sub pos (:vel @state/state))
-                     pos))))))))
+            (when (< n total-frames)
+              (recur (inc n)
+                     (if move?
+                       (vec2/sub pos (:vel @state/state))
+                       pos)))))))))
