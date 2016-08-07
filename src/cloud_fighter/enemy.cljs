cec13853d83e0c9ef926e00ff819e4360b7ccde2
Crispin Wellington <retrogradeorbit@gmail.com>
2016-08-07 19:36:23 +0800

    enemy shoots at you

--- src/cloud_fighter/enemy.cljs
+++ src/cloud_fighter/enemy.cljs
@@ -8,9 +8,16 @@
             [cloud-fighter.state :as state]
             [cloud-fighter.explosion :as explosion]
             [cloud-fighter.bullet :as bullet]
+            [cloud-fighter.missile :as missile]
             [cljs.core.async :refer [<! timeout]])
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [infinitelives.pixi.macros :as m]))
+
+(def bullet-speed 5)
+(def bullet-life 200)
+(def bullet-probability 0.001)
+(def missile-probability 0.001)
+(def missile-life 300)
 
 (defonce enemies (atom {}))
 
@@ -22,6 +29,37 @@
 
 (defn count-enemies []
   (count @enemies))
+
+(defonce enemy-bullets (atom {}))
+
+(defn remove-bullet! [bkey]
+  (swap! enemy-bullets dissoc bkey))
+
+(defn spawn-bullet! [canvas initial-pos heading speed lifetime]
+  (let [update (-> heading vec2/unit
+                   (vec2/scale speed))
+        bkey (keyword (gensym))
+        skey [:enemy-bullet bkey]]
+    (go
+      (m/with-sprite canvas :bullets
+        [bullet (s/make-sprite :bullet :scale 4 :x 0 :y 0)]
+        (swap! enemy-bullets assoc bkey bullet)
+        (spatial/add-to-spatial! :default skey (vec2/as-vector initial-pos))
+        (loop [n lifetime
+               pos initial-pos]
+          (s/set-pos! bullet pos)
+          (<! (e/next-frame))
+
+          (if (and (pos? n) (bkey @enemy-bullets))
+            (let [next-pos (vec2/sub (vec2/add pos update) (:vel @state/state))]
+              (spatial/move-in-spatial :default skey
+                                       (vec2/as-vector pos)
+                                       (vec2/as-vector next-pos))
+              (recur (dec n) next-pos))
+
+            (do
+              (remove! bkey)
+              (spatial/remove-from-spatial :default skey (vec2/as-vector pos)))))))))
 
 (defn spawn [canvas]
   (go
